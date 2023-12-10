3d9731eae2d794a96f3f9ee01fb655233e47d0c8
wy <2511933030@qq.com>
2023-12-10 18:26:22 +0800

    clear

---
+++ src/main/java/com/wy/springbootinit/esdao/PostEsDao.java
@@ -0,0 +1,15 @@
+package com.wy.springbootinit.esdao;
+
+import com.wy.springbootinit.model.dto.post.PostEsDTO;
+import java.util.List;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * 帖子 ES 操作
+ *
+
+ */
+public interface PostEsDao extends ElasticsearchRepository<PostEsDTO, Long> {
+
+    List<PostEsDTO> findByUserId(Long userId);
+}
---
+++ src/main/java/com/wy/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.wy.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wy.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
